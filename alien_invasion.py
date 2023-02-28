1f8de96e09e8bdc008cccc1d4f9b0352d79e1884
Ken Chen <cgx_2010@163.com>
2023-02-28 11:37:50 +0800

    添加飞船并在屏幕上绘制

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -20,6 +21,8 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+        
         # 设置背景颜色
         self.bg_color = (230, 230, 230)
 
@@ -36,6 +39,7 @@ class AlienInvasion:
 
             # 每次循环时都重绘屏幕
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # 让最近绘制的屏幕可见。
             pygame.display.flip()
